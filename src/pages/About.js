cab3a190ce527eed6c3d995330bc7ae814ee4855
Raymondkingjnr <raymondnnaji660@gmail.com>
2023-01-08 01:20:59 +0100

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis dolor
+        odio animi dolorum porro laboriosam itaque modi rem doloremque impedit
+        optio laudantium, commodi placeat inventore et exercitationem facere.
+        Aperiam consequatur ullam, deserunt cumque, rem perferendis reiciendis
+        ipsam, officia soluta exercitationem quasi optio consectetur aliquam
+        deleniti.
+      </p>
+    </section>
+  );
+};
+
+export default About;
